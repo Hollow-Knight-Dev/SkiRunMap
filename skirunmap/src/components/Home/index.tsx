85adb628daf44dff068f1e8abad3a279fec38dbd
Lily <yuyaoyao802@gmail.com>
2023-11-25 09:32:24 +0800

    fix: rename variable and order tailwind className

--- skirunmap/src/components/Home/index.tsx
+++ skirunmap/src/components/Home/index.tsx
@@ -5,27 +5,23 @@ import SnowMountain from './snow-mountain.png'
 const Home = () => {
   const [hasFilter, setHasFilter] = useState(false)
 
-  const handleClick = () => {
+  const handleFilterClick = () => {
     setHasFilter(true)
   }
 
-  const handleMouseLeave = () => {
+  const handleFilterMouseLeave = () => {
     setHasFilter(false)
   }
 
   return (
     <div className='flex flex-col items-center'>
       <div className='relative'>
-        <div className='flex flex-col items-center absolute top-24 left-1/3'>
-          <p className='font-bold text-3xl'>Find routes</p>
-          <div className='w-80 relative'>
-            <img
-              className='absolute w-7 top-2 left-4'
-              src={SearchIcon}
-              alt='Search Icon'
-            />
+        <div className='absolute left-1/3 top-24 flex flex-col items-center'>
+          <p className='text-3xl font-bold'>Find routes</p>
+          <div className='relative w-80'>
+            <img className='absolute left-4 top-2 w-7' src={SearchIcon} alt='Search Icon' />
             <input
-              className='w-full p-2 pl-16 border border-zinc-300 rounded-3xl'
+              className='w-full rounded-3xl border border-zinc-300 p-2 pl-16'
               placeholder='Ski resort, ski run, or tag name'
             />
           </div>
@@ -34,21 +30,21 @@ const Home = () => {
           <img src={SnowMountain} alt='Snow Mountain' />
         </div>
       </div>
-      <div className='w-fit p-8 flex flex-col items-center'>
-        <div className='w-full flex justify-between mb-2'>
-          <p className='font-bold text-3xl'>Hottest routes</p>
+      <div className='flex w-fit flex-col items-center p-8'>
+        <div className='mb-2 flex w-full justify-between'>
+          <p className='text-3xl font-bold'>Hottest routes</p>
           <div
-            className='flex items-center gap-2 relative'
-            onClick={handleClick}
-            onMouseLeave={handleMouseLeave}
+            className='relative flex items-center gap-2'
+            onClick={handleFilterClick}
+            onMouseLeave={handleFilterMouseLeave}
           >
-            <p className='font-bold text-xl'>filter</p>
+            <p className='text-xl font-bold'>filter</p>
             <svg
               xmlns='http://www.w3.org/2000/svg'
               fill='none'
               viewBox='0 0 24 24'
               stroke='currentColor'
-              className='h-full w-6 text-gray-600 cursor-pointer'
+              className='h-full w-6 cursor-pointer text-gray-600'
             >
               <path
                 strokeLinecap='round'
@@ -58,34 +54,28 @@ const Home = () => {
               />
             </svg>
             {hasFilter && (
-              <div className='flex flex-col items-center absolute top-8 right-0 bg-white shadow-lg rounded-md font-semibold w-20 pt-2 pb-2'>
-                <button className='w-full cursor-pointer hover:bg-zinc-100'>
-                  All
-                </button>
-                <button className='w-full cursor-pointer hover:bg-zinc-100'>
-                  Hottest
-                </button>
-                <button className='w-full cursor-pointer hover:bg-zinc-100'>
-                  Newest
-                </button>
+              <div className='absolute right-0 top-8 flex w-20 flex-col items-center rounded-md bg-white pb-2 pt-2 font-semibold shadow-lg'>
+                <button className='w-full cursor-pointer hover:bg-zinc-100'>All</button>
+                <button className='w-full cursor-pointer hover:bg-zinc-100'>Hottest</button>
+                <button className='w-full cursor-pointer hover:bg-zinc-100'>Newest</button>
               </div>
             )}
           </div>
         </div>
-        <div className='w-full border border-zinc-300 mb-6' />
-        <div className='w-fit grid grid-cols-4 gap-4'>
-          <div className='w-40 h-40 bg-zinc-300 rounded-2xl'></div>
-          <div className='w-40 h-40 bg-zinc-300 rounded-2xl'></div>
-          <div className='w-40 h-40 bg-zinc-300 rounded-2xl'></div>
-          <div className='w-40 h-40 bg-zinc-300 rounded-2xl'></div>
-          <div className='w-40 h-40 bg-zinc-300 rounded-2xl'></div>
-          <div className='w-40 h-40 bg-zinc-300 rounded-2xl'></div>
-          <div className='w-40 h-40 bg-zinc-300 rounded-2xl'></div>
-          <div className='w-40 h-40 bg-zinc-300 rounded-2xl'></div>
-          <div className='w-40 h-40 bg-zinc-300 rounded-2xl'></div>
-          <div className='w-40 h-40 bg-zinc-300 rounded-2xl'></div>
-          <div className='w-40 h-40 bg-zinc-300 rounded-2xl'></div>
-          <div className='w-40 h-40 bg-zinc-300 rounded-2xl'></div>
+        <div className='mb-6 w-full border border-zinc-300' />
+        <div className='grid w-fit grid-cols-4 gap-4'>
+          <div className='h-40 w-40 rounded-2xl bg-zinc-300'></div>
+          <div className='h-40 w-40 rounded-2xl bg-zinc-300'></div>
+          <div className='h-40 w-40 rounded-2xl bg-zinc-300'></div>
+          <div className='h-40 w-40 rounded-2xl bg-zinc-300'></div>
+          <div className='h-40 w-40 rounded-2xl bg-zinc-300'></div>
+          <div className='h-40 w-40 rounded-2xl bg-zinc-300'></div>
+          <div className='h-40 w-40 rounded-2xl bg-zinc-300'></div>
+          <div className='h-40 w-40 rounded-2xl bg-zinc-300'></div>
+          <div className='h-40 w-40 rounded-2xl bg-zinc-300'></div>
+          <div className='h-40 w-40 rounded-2xl bg-zinc-300'></div>
+          <div className='h-40 w-40 rounded-2xl bg-zinc-300'></div>
+          <div className='h-40 w-40 rounded-2xl bg-zinc-300'></div>
         </div>
       </div>
     </div>
