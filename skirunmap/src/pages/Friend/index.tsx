a11a8682b8d16bf1a5e4a62dc7b32773d179f873
Lily <yuyaoyao802@gmail.com>
2023-12-07 18:02:30 +0800

    feat: allow user to withdraw friend invitation on Friend Page

--- skirunmap/src/pages/Friend/index.tsx
+++ skirunmap/src/pages/Friend/index.tsx
@@ -14,7 +14,6 @@ import { toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { db } from '../../auth/CloudStorage'
 import { useUserStore } from '../../store/useUser'
-import ProfileIcon from './User-icon.png'
 import SearchIcon from './search-icon.png'
 
 interface UserSimpleData {
@@ -30,6 +29,7 @@ const Friends = () => {
   const [followerList, setFollowerList] = useState<UserSimpleData[]>()
   const [friendList, setFriendList] = useState<UserSimpleData[]>()
   const [friendReqList, setFriendReqList] = useState<UserSimpleData[]>()
+  const [sentFriendReqList, setSentFriendReqList] = useState<UserSimpleData[]>()
 
   useEffect(() => {
     if (isLoadedUserDoc && !isSignIn) {
@@ -76,9 +76,14 @@ const Friends = () => {
     setFriendList(friendsData)
   }
 
-  const retrieveFriendeqs = async (list: string[]) => {
+  const retrieveFriendReqs = async (list: string[]) => {
     const friendReqData = await retrieveUserSimpleData(list)
     setFriendReqList(friendReqData)
+  }
+
+  const retrieveSentFriendReqs = async (list: string[]) => {
+    const sentFriendReqData = await retrieveUserSimpleData(list)
+    setSentFriendReqList(sentFriendReqData)
   }
 
   useEffect(() => {
@@ -105,28 +110,28 @@ const Friends = () => {
   useEffect(() => {
     if (isLoadedUserDoc) {
       const userFriendReqList = userDoc.userFriendReqs
-      retrieveFriendeqs(userFriendReqList)
+      retrieveFriendReqs(userFriendReqList)
     }
   }, [userDoc])
-
-  useEffect(() => {
-    console.log('friendList:', friendList)
-    console.log('friendReqList:', friendReqList)
-  }, [friendReqList])
 
   useEffect(() => {
     if (isLoadedUserDoc) {
       const userFriendSentReqList = userDoc.userSentFriendReqs
+      retrieveSentFriendReqs(userFriendSentReqList)
     }
   }, [userDoc])
+
+  useEffect(() => {
+    console.log('sentFriendReqList:', sentFriendReqList)
+  }, [sentFriendReqList])
 
   const handleUnfollowFollows = async (id: string) => {
     const myUserRef = doc(db, 'users', userDoc.userID)
     const otherUserRef = doc(db, 'users', id)
     await updateDoc(myUserRef, { userFollows: arrayRemove(id) })
     await updateDoc(otherUserRef, { userFollowers: arrayRemove(userDoc.userID) })
-    const updateFollowList = followList?.filter((user) => user.userID !== id)
-    setFollowList(updateFollowList)
+    const updateList = followList?.filter((user) => user.userID !== id)
+    setFollowList(updateList)
   }
 
   const handleRemoveFollowers = async (id: string) => {
@@ -134,8 +139,8 @@ const Friends = () => {
     const otherUserRef = doc(db, 'users', id)
     await updateDoc(myUserRef, { userFollowers: arrayRemove(id) })
     await updateDoc(otherUserRef, { userFollows: arrayRemove(userDoc.userID) })
-    const updateFollowerList = followerList?.filter((user) => user.userID !== id)
-    setFollowerList(updateFollowerList)
+    const updateList = followerList?.filter((user) => user.userID !== id)
+    setFollowerList(updateList)
   }
 
   const handleBreakUpFriends = async (id: string) => {
@@ -143,12 +148,11 @@ const Friends = () => {
     const otherUserRef = doc(db, 'users', id)
     await updateDoc(myUserRef, { userFriends: arrayRemove(id) })
     await updateDoc(otherUserRef, { userFriends: arrayRemove(userDoc.userID) })
-    const updateFriendList = friendList?.filter((user) => user.userID !== id)
-    setFriendList(updateFriendList)
+    const updateList = friendList?.filter((user) => user.userID !== id)
+    setFriendList(updateList)
   }
 
   const handleAcceptFriendReqs = async (id: string) => {
-    console.log('Accept')
     const myUserRef = doc(db, 'users', userDoc.userID)
     const otherUserRef = doc(db, 'users', id)
     await updateDoc(myUserRef, { userFriends: arrayUnion(id) })
@@ -159,22 +163,31 @@ const Friends = () => {
     setFriendReqList(updateFriendReqList)
     const otherUserData = friendReqList?.find((user) => user.userID === id)
     if (friendList && otherUserData) {
-      const updateFriendList = [...friendList, otherUserData]
-      setFriendList(updateFriendList)
+      const updateList = [...friendList, otherUserData]
+      setFriendList(updateList)
     } else if (otherUserData) {
-      const updateFriendList = [otherUserData]
-      setFriendList(updateFriendList)
+      const updateList = [otherUserData]
+      setFriendList(updateList)
     }
   }
 
   const handleRejectFriendReqs = async (id: string) => {
-    console.log('Reject')
     const myUserRef = doc(db, 'users', userDoc.userID)
     const otherUserRef = doc(db, 'users', id)
     await updateDoc(myUserRef, { userFriendReqs: arrayRemove(id) })
     await updateDoc(otherUserRef, { userSentFriendReqs: arrayRemove(userDoc.userID) })
-    const updateFriendReqList = friendReqList?.filter((user) => user.userID !== id)
-    setFriendReqList(updateFriendReqList)
+    const updateList = friendReqList?.filter((user) => user.userID !== id)
+    setFriendReqList(updateList)
+  }
+
+  const handleWithdrawSentFriendReqs = async (id: string) => {
+    console.log('Withdraw Sent Friend Reqs')
+    const myUserRef = doc(db, 'users', userDoc.userID)
+    const otherUserRef = doc(db, 'users', id)
+    await updateDoc(myUserRef, { userSentFriendReqs: arrayRemove(id) })
+    await updateDoc(otherUserRef, { userFriendReqs: arrayRemove(userDoc.userID) })
+    const updateList = sentFriendReqList?.filter((user) => user.userID !== id)
+    setSentFriendReqList(updateList)
   }
 
   return (
@@ -297,11 +310,26 @@ const Friends = () => {
       <div className='mb-16'>
         <p className='mb-4 text-2xl font-bold'>Sent Invitation</p>
         <div className='mb-8 w-full border border-zinc-300' />
-        <div className='flex items-center justify-center'>
-          <div className='flex items-center'>
-            <img className='h-20 w-20' src={ProfileIcon} alt='Friend Profile Icon' />
-            <p className='w-40 bg-zinc-100 text-center'>I Am Not Groot</p>
-          </div>
+        <div className='flex flex-col gap-4'>
+          {sentFriendReqList &&
+            sentFriendReqList.map((user, index) => (
+              <div key={index} className='flex items-center justify-between'>
+                <Link to={`/member/${user.userID}`} className='flex items-center gap-4'>
+                  <img
+                    className='h-20 w-20 rounded-full object-cover'
+                    src={user.userIconUrl}
+                    alt='Friend Profile Icon'
+                  />
+                  <p className='text-xl'>{user.username}</p>
+                </Link>
+                <button
+                  className='h-10 w-20 rounded-xl bg-zinc-100 hover:bg-red-200'
+                  onClick={() => handleWithdrawSentFriendReqs(user.userID)}
+                >
+                  Withdraw
+                </button>
+              </div>
+            ))}
         </div>
       </div>
     </div>
