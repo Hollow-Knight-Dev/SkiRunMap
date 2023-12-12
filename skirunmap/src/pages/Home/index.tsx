c3303fe092484d6375889b1cd9b520d0a3d03805
Lily <yuyaoyao802@gmail.com>
2023-12-12 16:53:36 +0800

    feat: add images preview for viewing routes on Home page

--- skirunmap/src/pages/Home/index.tsx
+++ skirunmap/src/pages/Home/index.tsx
@@ -12,11 +12,13 @@ import { Link } from 'react-router-dom'
 import { db } from '../../auth/CloudStorage'
 import FilterSVG from '../../components/FilterSVG'
 import HeroHeader from '../../components/HeroHeader'
+import { Spot } from '../../store/useRoute'
 
 const Home = () => {
   const [allRoutes, setAllRoutes] = useState<DocumentData[]>([])
   const [hasFilter, setHasFilter] = useState(false)
   const [filter, setFilter] = useState<string>('All')
+  const [selectedImages, setSelectedImages] = useState<{ [routeID: string]: number }>({})
 
   const handleFilterIconClick = () => {
     setHasFilter((prev) => !prev)
@@ -31,7 +33,7 @@ const Home = () => {
   }
 
   useEffect(() => {
-    console.log(filter)
+    // console.log(filter)
     const filterOptions: Record<string, QueryOrderByConstraint | null> = {
       Newest: orderBy('createTime', 'desc'),
       'Most likes': orderBy('likeCount', 'desc'),
@@ -64,6 +66,26 @@ const Home = () => {
 
     getRoutes()
   }, [filter])
+
+  useEffect(() => {
+    const initialSelectedImages: { [routeID: string]: number } = {}
+    allRoutes.forEach((route) => {
+      initialSelectedImages[route.routeID] = 0
+    })
+    setSelectedImages(initialSelectedImages)
+  }, [allRoutes])
+
+  // useEffect(() => {
+  //   console.log('selectedImages:', selectedImages)
+  // }, [selectedImages])
+
+  const handleDotClick = (routeID: string, index: number) => {
+    setSelectedImages((prev) => ({ ...prev, [routeID]: index }))
+  }
+
+  // const handleImageBug = (index: number) => {
+  //   console.log(index)
+  // }
 
   return (
     <div className='flex w-full flex-col items-center'>
@@ -99,26 +121,94 @@ const Home = () => {
           )}
         </div>
         <div className='mb-6 w-full border border-zinc-300' />
-        <div className='flex w-fit flex-wrap gap-4'>
-          {allRoutes.map((map, index) => (
-            <Link
-              key={index}
-              to={`/route/${map.routeID}`}
-              className='h-48 w-48 cursor-pointer rounded-2xl bg-zinc-300 p-4'
-            >
-              <p>Title: {map.routeTitle}</p>
-              <p>User: {map.username}</p>
-              <p>Tag: {map.tags}</p>
-              <p>Snow Buddy: {map.snowBuddies}</p>
-              <p>LikeCount: {map.likeCount}</p>
-              <p>viewCount: {map.viewCount}</p>
-              <div className='flex gap-1'>
-                {map.spots[0].imageUrls.map((url: string, index: number) => (
-                  <img key={index} src={url} alt={`Image ${index}`} className='h-auto w-6' />
-                ))}
+        <div className='flex w-full flex-col flex-wrap gap-4'>
+          {allRoutes.map((route, index) => {
+            let imageIndex = 0
+            return (
+              <div
+                key={index}
+                className='relative h-60 w-full cursor-pointer rounded-2xl bg-zinc-100 p-4'
+              >
+                <Link
+                  key={`${route.routeID}_${index}`}
+                  to={`/route/${route.routeID}`}
+                  className='absolute left-0 top-0 z-10 h-full w-full cursor-pointer rounded-2xl'
+                />
+                <div className='flex gap-8 text-xl'>
+                  <div className='relative h-52 w-52 rounded-xl bg-zinc-200'>
+                    <div className='flex h-full w-full'>
+                      {route.spots?.map((spot: Spot) =>
+                        spot.imageUrls.map((url: string) => (
+                          <img
+                            key={imageIndex++}
+                            src={url}
+                            alt={spot.spotTitle}
+                            className={`aspect-square rounded-xl object-cover ${
+                              selectedImages[route.routeID] === imageIndex ? 'block' : 'hidden'
+                            }`}
+                            // onClick={() => handleImageBug(imageIndex)}
+                          />
+                        ))
+                      )}
+                    </div>
+                    <div className='absolute bottom-2 z-20 flex h-4 w-full flex-wrap items-center justify-center gap-2'>
+                      {Array.from({
+                        length: route.spots?.reduce(
+                          (acc: number, spot: Spot) => acc + spot.imageUrls.length,
+                          0
+                        )
+                      }).map((_, spanIndex) => (
+                        <span
+                          key={spanIndex}
+                          className={`dot h-2 w-2 rounded-full  opacity-70 ${
+                            selectedImages[route.routeID] === spanIndex ? 'bg-blue-500' : 'bg-white'
+                          }`}
+                          onClick={() => handleDotClick(route.routeID, spanIndex)}
+                        />
+                      ))}
+                    </div>
+                  </div>
+
+                  <div className='flex flex-col gap-4'>
+                    <p className='font-bold'>Route Title: {route.routeTitle}</p>
+                    <p>Likes: {route.likeCount}</p>
+                    <div className='flex flex-wrap gap-2'>
+                      <p>Tag: </p>
+                      {route.tags.length > 0 ? (
+                        route.tags.map((tag: string, index: number) => (
+                          <Link
+                            key={`${route.routeID}_tag_${index}`}
+                            to={`/search/${tag}`}
+                            className='rounded-xl bg-blue-100 pl-2 pr-2'
+                          >
+                            # {tag}
+                          </Link>
+                        ))
+                      ) : (
+                        <p>None</p>
+                      )}
+                    </div>
+                    <div className='flex flex-wrap gap-2'>
+                      <p>Spots: </p>
+                      {route.spots.length > 0 ? (
+                        route.spots?.map((spot: Spot, index: number) => (
+                          <Link
+                            key={`${route.routeID}_spot_${index}`}
+                            to={`/search/${spot.spotTitle}`}
+                            className='rounded-xl bg-blue-100 pl-2 pr-2'
+                          >
+                            {spot.spotTitle}
+                          </Link>
+                        ))
+                      ) : (
+                        <p>None</p>
+                      )}
+                    </div>
+                  </div>
+                </div>
               </div>
-            </Link>
-          ))}
+            )
+          })}
         </div>
       </div>
     </div>
