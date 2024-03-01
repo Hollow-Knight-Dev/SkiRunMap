6e59c2ca77234b2a1d003b54245a6c03b5d7d396
Lily <yuyaoyao802@gmail.com>
2024-03-01 17:18:23 +0800

    chore: alter local port and vite config

--- skirunmap/vite.config.ts
+++ skirunmap/vite.config.ts
@@ -4,5 +4,8 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.gpx', '**/*.PNG']
+  assetsInclude: ['**/*.gpx', '**/*.PNG'],
+  server: {
+    port: 1024
+  }
 })
